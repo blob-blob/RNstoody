4f17cda63aca60bca2602d2c810eea1b9b39524b
jyko <yeon0125@gmail.com>
2022-04-03 04:35:30 +0900

    setState2

--- App.js
+++ App.js
@@ -10,14 +10,16 @@ import { View, Text, StyleSheet } from 'react-native';
  */
 
 class App extends Component {
-    // render 함수 밖에 선언.
     state = {
         sampleText: 'hello world',
         sampleBoolean: false,
+        sampleNum: 1,
     };
 
     // 화면에 변경된 값을 반영하려면 setState 이용.
     // setState를 이용해서 state를 갱신될때 다시 렌더링. state를 렌더링 데이터를 관리하기 때문에 직접 변경될 수 없어야하는 특성.
+    // 비동기
+
     intputText = () => (this.state.sampleBoolean ? <Text>sampleBoolean is true</Text> : <Text>sampleBoolean is false</Text>);
 
     changeState = () => {
@@ -33,10 +35,26 @@ class App extends Component {
             });
         }
     };
+
+    onAdd = () => {
+        /**
+        this.setState({
+            sampleNum: sampleNum + 1
+        }) // 이렇게 하면 에러남.
+
+        현재 버전을 copy하고 update를 하는거라서 안되는 것.
+        현재 버전의 값을 넘겨주고, 두번째 콜백함수에서 연산처리 하도록.
+    */
+        this.setState(prevState => {
+            return {
+                sampleNum: prevState.sampleNum + 1,
+            };
+        });
+    };
     render() {
         return (
             <View style={styles.background}>
-                <Text onPress={this.changeState}>{this.state.sampleText}</Text>
+                <Text onPress={this.onAdd}>{this.state.sampleNum}</Text>
             </View>
         );
     }
