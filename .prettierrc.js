0da03ea7ef385e4a5f61b0ad2b6c5a4520a8a777
jyko <yeon0125@gmail.com>
2022-04-03 03:35:10 +0900

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 160,
+  tabWidth: 4,
 };
