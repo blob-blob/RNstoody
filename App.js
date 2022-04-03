69898de62c73bc285d18a8d16b5fd02897ba2b07
jyko <yeon0125@gmail.com>
2022-04-03 22:11:58 +0900

    touchEvent

--- App.js
+++ App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from './src/header';
 class App extends Component {
+    state = {
+        appName: 'My first app',
+    };
     render() {
         return (
-            // 화면에 출력하기 위해서는 뷰 컴포넌트로 감싸져있어야 함.
-            // 인라인 스타일 or 따로 빼서 하는 스타일
             <View style={styles.mainView}>
-                <View
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    style={{
-                        backgroundColor: 'green',
-                        // marginTop: 50,
-                        paddingTop: 50,
-                    }}>
-                    <Text style={styles.mainText}>hello world</Text>
-                </View>
-                <View
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    style={{
-                        backgroundColor: 'green',
-                        marginTop: 50,
-                        // paddingTop: 50,
-                    }}>
-                    <Text>hello world</Text>
-                </View>
+                <Header name={this.state.appName} />
                 <View style={styles.subView}>
-                    <Text>hello world</Text>
+                    <Text style={styles.mainText}>hello world</Text>
                 </View>
             </View>
         );
@@ -40,15 +25,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center', // 수직정렬
     },
     subView: {
-        // flex: 2,
         backgroundColor: 'yellow',
         marginBottom: 10,
-        width: '100%',
         alignItems: 'center',
     },
     mainText: {
         fontSize: 20,
-        fontWeight: 'bold',
+        fontWeight: 'normal',
         color: 'red',
         padding: 20,
     },
---
+++ src/header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+
+/**
+    exampleFunction1 = () => {
+    // 리턴되는 jsx컴포넌트가 없음
+    }
+
+    exampleFunction2 = () => (
+    // 리턴되는 jsx컴포넌트가 있음
+    // 태그에 감싸져서 변수에 할당되는 표현식
+    )
+ */
+
+const Header = props => (
+    // eslint-disable-next-line no-alert
+    // <TouchableWithoutFeedback onPress={() => alert('hello')}>
+    //     <View style={styles.header}>
+    //         <Text>{props.name}</Text>
+    //     </View>
+    // </TouchableWithoutFeedback>
+
+    // <TouchableOpacity style={styles.header} onLongPress={() => alert('hello')}>
+    // <TouchableOpacity style={styles.header} onPressIn={() => alert('hello')}> {/* 클릭을 하는 순간 이벤트 발생 */}
+    // <TouchableOpacity style={styles.header} onPressOut={() => alert('hello')}> {/* 클릭을 놓는 순간 이벤트 발생 */}
+    //     <View>
+    //         <Text>{props.name}</Text>
+    //     </View>
+    // </TouchableOpacity>
+
+    <TouchableOpacity style={styles.header} onPressOut={() => alert('hello')}>
+        <View>
+            <Text>{props.name}</Text>
+        </View>
+    </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        padding: 5,
+        width: '100%',
+    },
+});
+
+export default Header;
