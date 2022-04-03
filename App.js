3adff6f7d4c773b7ac7f2b8984152b31f4929dc7
jyko <yeon0125@gmail.com>
2022-04-03 21:33:17 +0900

    style2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ class App extends Component {
         return (
             // 화면에 출력하기 위해서는 뷰 컴포넌트로 감싸져있어야 함.
             // 인라인 스타일 or 따로 빼서 하는 스타일
-            <View style={styles.background}>
+            <View style={styles.mainView}>
                 <View
                     // eslint-disable-next-line react-native/no-inline-styles
                     style={{
@@ -13,7 +13,7 @@ class App extends Component {
                         // marginTop: 50,
                         paddingTop: 50,
                     }}>
-                    <Text>hello world</Text>
+                    <Text style={styles.mainText}>hello world</Text>
                 </View>
                 <View
                     // eslint-disable-next-line react-native/no-inline-styles
@@ -24,7 +24,7 @@ class App extends Component {
                     }}>
                     <Text>hello world</Text>
                 </View>
-                <View>
+                <View style={styles.subView}>
                     <Text>hello world</Text>
                 </View>
             </View>
@@ -33,11 +33,24 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-    background: {
+    mainView: {
         flex: 1,
         backgroundColor: 'fff',
         alignItems: 'center',
         justifyContent: 'center', // 수직정렬
+    },
+    subView: {
+        // flex: 2,
+        backgroundColor: 'yellow',
+        marginBottom: 10,
+        width: '100%',
+        alignItems: 'center',
+    },
+    mainText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'red',
+        padding: 20,
     },
 });
 
