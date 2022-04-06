3e7729d8cf29f2bd2784682986addfb284934f13
jyko <yeon0125@gmail.com>
2022-04-06 22:39:21 +0900

    ~redux setting (reducer, enhancer, __REDUX_DEVTOOLS_EXTENSION_COMPOSE__)

--- index.js
+++ index.js
@@ -14,15 +14,23 @@ import { Provider } from 'react-redux';
 // 스토어를 리액트네이티브 컴포넌트로 패싱해주기 위해 필요함.
 import promiseMiddleware from 'redux-promise';
 // 비동기가 필요한 액션크리에이터를 위함
+import reducers from './app/store/reducers';
 
-const StoreWithMiddelware = createStore([reducer], [enhancer]);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// redux 개발자도구와 미들웨어를 사용하기 위해
+// __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: 크롬 확장프로그램에 작성되어있는 자바스크립트 함수.
 
-const appRedux = () => {
+const StoreWithMiddelware = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(promiseMiddleware)),
+);
+
+const appRedux = () => (
     <Provider store={StoreWithMiddelware}>
         {/* 앱 전체를 store를 prop으로 갖는 provider로 감싸줌. 이 store를 사용할 것임을 알려줌 */}
         <ReduxApp />
-    </Provider>;
-};
+    </Provider>
+);
 
 // AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent(appName, () => appRedux);
