4f951fb8327044cad3f3702dbf94d2b7bf3b402e
jyko <yeon0125@gmail.com>
2022-04-09 08:50:11 +0900

    input authForm

--- app/components/auth/authForm.js
+++ app/components/auth/authForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
-
+import Input from '../../utils/forms/input';
 class AuthForm extends Component {
     state = {
         myTextInput: '',
@@ -9,37 +9,32 @@ class AuthForm extends Component {
     render() {
         return (
             <View>
-                <TextInput
+                <Input
                     value={this.state.myTextInput}
                     autoCapitalize={'none'}
                     keyboardType={'email-address'}
-                    style={styles.input}
                     placeholder="이메일 주소"
                     placeholderTextColor="#ddd"
                 />
 
-                <TextInput
+                <Input
                     value={this.state.myTextInput}
                     autoCapitalize={'none'}
                     keyboardType={'email-address'}
-                    style={styles.input}
                     placeholder="비밀번호"
+                    placeholderTextColor="#ddd"
+                />
+
+                <Input
+                    value={this.state.myTextInput}
+                    autoCapitalize={'none'}
+                    keyboardType={'email-address'}
+                    placeholder="비밀번호 재확인"
                     placeholderTextColor="#ddd"
                 />
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    input: {
-        width: '100%',
-        borderBottomWidth: 1,
-        borderBottomColor: '#fff',
-        fontSize: 17,
-        padding: 5,
-        marginTop: 30,
-    },
-});
 
 export default AuthForm;
