824763c43e5f30d89572fcd9e75b6a8bfc5f8237
jyko <yeon0125@gmail.com>
2022-04-12 00:03:09 +0900

    login redux1

--- app/store/reducers/index.js
+++ app/store/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import Sample from './sample_reducer';
+import User from './user_reducer';
 
 const rootReducer = combineReducers({
-    Sample,
+    User,
 });
 export default rootReducer;
 // 앞으로 만들어갈 리듀서를 여기서 묶을것.
